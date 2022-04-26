8e7af1e9e84c4d5b408878e6796c38c6d0ed353c
Jake Fuentes <jake@jakef.com>
2022-04-26 16:23:38 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,26 +11,28 @@ ticker = st.text_input("Enter ticker symbol")
 
 clicked = st.button("Get Prices")
 
-if ticker:
+if clicked:
 
-  base_url = "https://query1.finance.yahoo.com/v7/finance/download/"
-  url_args= "?period1=1619473704&period2=1651009704&interval=1d&events=history&includeAdjustedClose=true"
-  url = base_url + ticker + url_args
-  
-  try:
-    df = pd.read_csv(url)
-  except:
-    st.error("No data from that ticker. Try again, Jon!")
-    st.stop()
-  if df.empty:
-    st.error("No data from that ticker. Try again, Jon!")
+  if ticker:
+
+    base_url = "https://query1.finance.yahoo.com/v7/finance/download/"
+    url_args= "?period1=1619473704&period2=1651009704&interval=1d&events=history&includeAdjustedClose=true"
+    url = base_url + ticker + url_args
+
+    try:
+      df = pd.read_csv(url)
+    except:
+      st.error("No data from that ticker. Try again, Jon!")
+      st.stop()
+    if df.empty:
+      st.error("No data from that ticker. Try again, Jon!")
+    else:
+      price = df[['Date','Close']]
+      price['Date'] = price['Date'].astype('datetime64[D]')
+      price = price.set_index('Date')
+      last_close = price.iloc[0,0]
+      st.metric("Last Close Price", last_close)
+
+      st.line_chart(price)
   else:
-    price = df[['Date','Close']]
-    price['Date'] = price['Date'].astype('datetime64[D]')
-    price = price.set_index('Date')
-    last_close = price.iloc[0,0]
-    st.metric("Last Close Price", last_close)
-    
-    st.line_chart(price)
-else:
-  st.error("Needs a ticker, Jon!")
+    st.error("Needs a ticker, Jon!")
